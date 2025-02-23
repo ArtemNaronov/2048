3f55e1db6576c46dbc1516a63f39fa92ea260664
ArtemNaronov <temick1995@yandex.ru>
2025-02-23 22:30:28 +0400

    добавил поддержку свайпов

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -93,28 +93,3 @@ export const moveTiles = (board, direction) => {
 
   return { board: newBoard, score };
 };
-
-// Управление свайпами
-let xDown = null;
-let yDown = null;
-
-export function handleTouchStart(evt) {
-  xDown = evt.touches[0].clientX;
-  yDown = evt.touches[0].clientY;
-}
-
-export function handleTouchMove(evt) {
-  if (!xDown || !yDown) return;
-
-  let xUp = evt.touches[0].clientX;
-  let yUp = evt.touches[0].clientY;
-
-  let xDiff = xDown - xUp;
-  let yDiff = yDown - yUp;
-
-  moveTiles(xDiff > 0 ? "ArrowLeft" : "ArrowRight");
-  moveTiles(yDiff > 0 ? "ArrowUp" : "ArrowDown");
-
-  xDown = null;
-  yDown = null;
-}
